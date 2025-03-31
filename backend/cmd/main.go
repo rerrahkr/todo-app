0a27339b6f20351d2fd1de75e3dff83cd7402154
Rerrah <rerrah57@gmail.com>
2025-03-31 14:40:19 +0900

    [Backend] Add DB connection controls

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"todoapp-backend/internal/db"
+)
 
 func main() {
+	if err := db.Connect(); err != nil {
+		log.Panicln("Error connecting to database:", err)
+	}
+	log.Println("Connected to database")
+
+	defer func() {
+		db.Disconnect()
+		log.Println("Disconnected from database")
+	}()
+
+	if err := db.Ping(); err != nil {
+		log.Panicln("Unable to ping database:", err)
+	}
+
 	fmt.Println("Hello, World!")
 }
