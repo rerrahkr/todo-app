e7c4a1c25877a5eb063753d587dcf2e9f89c368e
Rerrah <rerrah57@gmail.com>
2025-04-19 17:16:02 +0900

    [Frontend] Make list wireframe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Masonry from "@mui/lab/Masonry";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import type { SxProps, Theme } from "@mui/material/styles";
+import type React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+	id: number;
+	content: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type CardItemProps = {
+	cardId: number;
+	content: string;
+	onCheck: (id: number) => void;
+	onClick: (id: number) => void;
+	sx?: SxProps<Theme>;
+};
+
+function CardItem({
+	cardId,
+	content,
+	onCheck,
+	onClick,
+	sx,
+}: CardItemProps): React.JSX.Element {
+	return (
+		<Card
+			sx={{
+				...sx,
+				display: "flex",
+			}}
+		>
+			<CardActions
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+				}}
+			>
+				<Checkbox onChange={() => onCheck(cardId)} />
+			</CardActions>
+			<CardActionArea onClick={() => onClick(cardId)}>
+				<CardContent>
+					<Typography variant="body1">{content}</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
 }
 
-export default App
+function* range(begin: number, end: number, step = 1): Generator<number> {
+	for (let i = begin; i < end; i += step) {
+		yield i;
+	}
+}
+
+const TODOS: Todo[] = [...range(1, 11)].map(
+	(i): Todo => ({
+		id: i,
+		content: `Todo ${i}`,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	}),
+);
+
+function handleChecked(cardId: number) {}
+
+function handleClicked(cardId: number) {}
+
+function App() {
+	return (
+		<>
+			<CssBaseline />
+			<Container sx={{ my: 4 }}>
+				<Masonry columns={4} spacing={2}>
+					{TODOS.map((todo) => (
+						<CardItem
+							key={todo.id}
+							cardId={todo.id}
+							content={todo.content}
+							onCheck={handleChecked}
+							onClick={handleClicked}
+						/>
+					))}
+				</Masonry>
+			</Container>
+		</>
+	);
+}
+
+export default App;
