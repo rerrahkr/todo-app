0efd0fca4a4bdc8f30db6c2ce69266d31c5f78f4
Rerrah <rerrah57@gmail.com>
2025-04-20 11:13:28 +0900

    [Frontend] Add todo edit dialog
    still not call API yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Masonry from "@mui/lab/Masonry";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
@@ -6,9 +7,16 @@ import CardContent from "@mui/material/CardContent";
 import Checkbox from "@mui/material/Checkbox";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import type { SxProps, Theme } from "@mui/material/styles";
 import type React from "react";
+import { useState, useTransition } from "react";
+import { z } from "zod";
 
 type Todo = {
 	id: number;
@@ -72,11 +80,89 @@ const TODOS: Todo[] = [...range(1, 11)].map(
 	}),
 );
 
-function handleChecked(cardId: number) {}
+type EditTodoDialogProps = {
+	todo: Todo;
+	open: boolean;
+	onClose: () => void;
+};
 
-function handleClicked(cardId: number) {}
+const editTodoPayloadSchema = z.object({
+	content: z.string().min(1),
+});
 
-function App() {
+function EditTodoDialog({
+	todo,
+	open,
+	onClose,
+}: EditTodoDialogProps): React.JSX.Element {
+	const [failedValidation, setFailedValidation] = useState<boolean>(false);
+
+	const [isPending, startTransition] = useTransition();
+
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		startTransition(async () => {
+			const formData = new FormData(e.currentTarget);
+			const formObject = Object.fromEntries(formData.entries());
+			const parseResult = editTodoPayloadSchema.safeParse(formObject);
+
+			if (!parseResult.success) {
+				setFailedValidation(true);
+				return;
+			}
+
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+
+			setFailedValidation(false);
+			onClose();
+		});
+	}
+
+	const hasError = !isPending && failedValidation;
+
+	return (
+		<Dialog open={open} onClose={onClose}>
+			<form onSubmit={handleSubmit}>
+				<DialogTitle>Edit Todo Item</DialogTitle>
+				<DialogContent>
+					<TextField
+						id="content"
+						label="Content"
+						name="content"
+						variant="standard"
+						defaultValue={todo.content}
+						error={hasError}
+						helperText={hasError && "Invalid content."}
+						disabled={isPending}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={onClose}>Cancel</Button>
+					<Button type="submit" disabled={isPending}>
+						{isPending ? "Saving..." : "Save"}
+					</Button>
+				</DialogActions>
+			</form>
+		</Dialog>
+	);
+}
+
+function App(): React.JSX.Element {
+	const [todo, setTodo] = useState<Todo>();
+
+	const [dialogIsOpened, setDialogIsOpened] = useState<boolean>(false);
+
+	function handleChecked(cardId: number) {}
+
+	function handleClicked(cardId: number) {
+		setTodo(TODOS.find((todo) => todo.id === cardId));
+		setDialogIsOpened(true);
+	}
+
+	function handleCloseDialog() {
+		setDialogIsOpened(false);
+	}
+
 	return (
 		<>
 			<CssBaseline />
@@ -93,6 +179,13 @@ function App() {
 					))}
 				</Masonry>
 			</Container>
+			{todo && (
+				<EditTodoDialog
+					todo={todo}
+					open={dialogIsOpened}
+					onClose={handleCloseDialog}
+				/>
+			)}
 		</>
 	);
 }
