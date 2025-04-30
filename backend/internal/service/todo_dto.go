6c39e8bf02b5dccac58bbff9fb7f50db6d826a45
Rerrah <rerrah57@gmail.com>
2025-04-30 20:07:17 +0900

    [Backend] Change API JSON keys to camel-case

--- backend/internal/service/todo_dto.go
+++ backend/internal/service/todo_dto.go
@@ -5,8 +5,8 @@ import "time"
 type Todo struct {
 	ID        int       `json:"id"`
 	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type NewTodoRequest struct {
